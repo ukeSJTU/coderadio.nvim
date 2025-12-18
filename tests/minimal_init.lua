f33263421af932e66ad2e5b65cc9d815b541434c
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2025-12-18 22:54:25 +0800

    test: update minimal test configuration

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,7 +1,7 @@
 local plenary_dir = os.getenv("PLENARY_DIR") or "/tmp/plenary.nvim"
 local is_not_a_directory = vim.fn.isdirectory(plenary_dir) == 0
 if is_not_a_directory then
-  vim.fn.system({"git", "clone", "https://github.com/nvim-lua/plenary.nvim", plenary_dir})
+  vim.fn.system({ "git", "clone", "https://github.com/nvim-lua/plenary.nvim", plenary_dir })
 end
 
 vim.opt.rtp:append(".")
@@ -9,3 +9,6 @@ vim.opt.rtp:append(plenary_dir)
 
 vim.cmd("runtime plugin/plenary.vim")
 require("plenary.busted")
+
+-- Load coderadio plugin
+vim.cmd("runtime plugin/coderadio.lua")
